b50b8e2fcff791d5af8e7d0eb6bdd75185f16859
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-12-03 08:50:16 -0600

    Add day 2 files

---
+++ day-2/one.hs
@@ -0,0 +1,97 @@
+import Data.Maybe (fromJust)
+import Data.Text (splitOn, unpack)
+import Data.String (fromString)
+
+type Counts = (Integer, Integer, Integer)
+
+main = do
+    input <- getContents
+    let total = sum $ [id | (id, game) <- games, isPossible game]
+        games = map parse (lines input) in
+            print total
+
+parse :: String -> (Integer, [Counts])
+parse s =
+    let id   = parseNumber $ head parts
+        game = parseGame   $ last parts
+    in
+        (fromJust id, game)
+    where
+        parts  = map unpack parts'
+        parts' = splitOn sep s'
+        s'     = fromString s
+        sep    = fromString ": "
+
+        parseNumber :: String -> Maybe Integer
+        parseNumber = parseNumber' Nothing where
+
+            parseNumber' :: Maybe Integer -> String -> Maybe Integer
+            parseNumber' previous "" = previous
+            parseNumber' previous (x:xs) =
+                let digit = parseDigit x
+                in
+                    case previous of
+                        Nothing -> parseNumber' digit xs
+                        Just n  -> case digit of
+                            Nothing -> Just n
+                            Just digit -> parseNumber' (Just $ 10*n + digit) xs
+                where
+                    parseDigit :: Char -> Maybe Integer
+                    parseDigit d = case d of
+                        '0' -> Just 0
+                        '1' -> Just 1
+                        '2' -> Just 2
+                        '3' -> Just 3
+                        '4' -> Just 4
+                        '5' -> Just 5
+                        '6' -> Just 6
+                        '7' -> Just 7
+                        '8' -> Just 8
+                        '9' -> Just 9
+                        _   -> Nothing
+
+        parseGame :: String -> [Counts]
+        parseGame s =
+            let rounds  = map unpack rounds'
+                rounds' = splitOn roundSep s'
+            in
+                map parseRound rounds
+            where
+                roundSep = fromString "; "
+                s'       = fromString s
+
+        parseRound :: String -> Counts
+        parseRound s =
+            let counts = map toCountAndColor parts
+                parts  = map unpack parts'
+                parts' = splitOn colorSep s'
+            in
+                parseRound' (0, 0, 0) counts
+            where
+                colorSep = fromString ", "
+                s'       = fromString s
+
+                parseRound' :: Counts -> [(Integer, String)] -> Counts
+                parseRound' counts [] = counts
+                parseRound' (r,g,b) ((n,color):xs) =
+                    case color of
+                        "red"   -> parseRound' (r+n, g, b) xs
+                        "green" -> parseRound' (r, g+n, b) xs
+                        "blue"  -> parseRound' (r, g, b+n) xs
+
+                toCountAndColor :: String -> (Integer, String)
+                toCountAndColor s =
+                    let count = fromJust $ parseNumber (head parts)
+                        color = last parts
+                    in
+                        (count, color)
+                    where
+                        parts  = map unpack parts'
+                        parts' = splitOn sep s'
+                        s'     = fromString s
+                        sep    = fromString " "
+
+isPossible :: [Counts] -> Bool
+isPossible [] = True
+isPossible (x@(r,g,b):xs) = not (r > 12 || g > 13 || b > 14)
+                            && isPossible xs
